b09ac40757a81e02941d14d37e9e7ceddef78fd1
ArtiomC <artiom.tomas@gmail.com>
2020-10-30 16:37:10 +0200

    Combinig commits

--- main/src/Main.java
+++ main/src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Task 5: change 1 from Branch1");
         System.out.println("Task 6: change 1 from Branch2");
+        System.out.println("Task 8: change 1 from Branch3");
+        System.out.println("Task 8: change 2 from Branch3");
+        System.out.println("Task 8: change 3 from Branch3");
         SecondMethod();
     }
 
