c5ec6946cdb62bf41525fcc1089f6b0d51d26f0b
ArtiomC <artiom.tomas@gmail.com>
2020-10-30 16:10:49 +0200

    Task 7 commit 1 from Branch1

--- main/src/Main.java
+++ main/src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Task 5: change 1 from Branch1");
         System.out.println("Task 6: change 1 from Branch2");
+        SecondMethod();
+    }
+
+    public static void SecondMethod(){
+        System.out.println("Changed");
         MyMethod(4, 5);
     }
 
